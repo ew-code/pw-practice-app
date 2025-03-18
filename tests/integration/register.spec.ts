c2d95c26d57a4ef8566a8940230d802036a3a89d
Ewelina <130902@gmail.com>
2025-03-18 20:06:16 +0100

    feat: add test for user registration with mismatched passwords

--- tests/integration/register.spec.ts
+++ tests/integration/register.spec.ts
@@ -34,5 +34,21 @@ test.describe("User Registration Tests", () => {
     await expect(authPage.emailErrorMessage).toBeVisible();
   });
 
+  test("User cannot register with mismatched passwords", async ({page}) => {
+    // Arrange
+    const newUserData = prepareRandomUser();
+    const mismatchedPassword = "differentPassword";
 
+    // Act
+    await authPage.navigateToRegister();
+    await authPage.fullNameInput.fill(newUserData.fullName);
+    await authPage.emailInput.fill(newUserData.email);
+    await authPage.passwordInput.fill(newUserData.password);
+    await authPage.repeatPasswordInput.fill(mismatchedPassword);
+    await authPage.agreementCheckbox.click();
+    // await authPage.registerButton.click();
+
+    // Assert
+    await expect(page.locator("input#input-re-password")).toHaveClass(/status-danger/);//password validation is missing
+  });
 });
