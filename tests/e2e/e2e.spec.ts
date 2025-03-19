2bef57f552d6c8c9a5e756c85af426c9aa22dfd3
Ewelina <130902@gmail.com>
2025-03-19 12:31:51 +0100

    refactor: enhance test descriptions for clarity and improve logout comment in end-to-end tests

--- tests/e2e/e2e.spec.ts
+++ tests/e2e/e2e.spec.ts
@@ -1,13 +1,11 @@
 import test, { expect } from "@playwright/test";
 import { AuthPage } from "../../src/pages/auth.page";
 import { DashboardPage } from "../../src/pages/dashboard.page";
-import prepareRandomUser from "../../src/factories/user.factory";
 import { appLoginCredentials } from "../../src/test-data/login.data";
 
 test.describe("End-to-End Tests", () => {
   let authPage: AuthPage;
   let dashboardPage: DashboardPage;
-  const loginCredentials = appLoginCredentials;
 
   test.beforeEach(async ({ page }) => {
     authPage = new AuthPage(page);
@@ -15,16 +13,16 @@ test.describe("End-to-End Tests", () => {
   });
 
   test.afterEach(async () => {
-    await authPage.logOut();//after clicking log out nothing happens
+    await authPage.logOut();// 1) after clicking log out nothing happens
   });
 
-test("User can log in, check security cameras", async () => {
+test("User can log in and check security cameras", async () => {
 
     await test.step("Log in with the new user", async () => {
       await authPage.logInUser(appLoginCredentials);
     });
   
-    await test.step('Check security cameras', async () => {
+    await test.step('Check all security cameras are visible', async () => {
       await dashboardPage.checkAllCamerasVisible();
    });
 });
@@ -36,7 +34,7 @@ test("User can register, log in, perform actions on core widgets", async () => {
       await authPage.registerAndLogInUser(appLoginCredentials);
   });
 
-    await test.step('Perform actions on the dashboard', async () => {
+    await test.step('Interact with IoT dashboard widgets', async () => {
       await dashboardPage.toggleLightSwitch();
       await dashboardPage.toggleRollerShades();
       await dashboardPage.toggleAudio();
